3439d71e95b90ff4640c31f89fd4197badb0c61f
pooya parsa <pooya@pi0.ir>
2020-04-15 18:02:36 +0200

    chore: simplify example/nuxt.config.js

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -1,11 +1,9 @@
-import { resolve } from 'path'
 import colorModeModule from '..'
 
 export default {
-  rootDir: resolve(__dirname, '..'),
-  buildDir: resolve(__dirname, '.nuxt'),
-  srcDir: __dirname,
-  css: ['@/assets/main.css'],
+  css: [
+    '@/assets/main.css'
+  ],
   modules: [
     '@nuxtjs/svg',
     colorModeModule
