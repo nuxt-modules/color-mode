078d38f6347a297f388ef65aedfa503892bf73b0
Jefry Dewangga <jefrydco@gmail.com>
2020-09-14 22:36:02 +0700

    feat: add types declaration (#27)
    Close #15

---
+++ types/color-mode.d.ts
@@ -0,0 +1,46 @@
+export interface ColorModeCookieOptions {
+  /**
+   * Default: `nuxt-color-mode`
+   */
+  key: string,
+  options: {
+    /**
+     * Default: `nuxt.options.router.base`
+     */
+    path: string,
+    /**
+     * Default: `lax`
+     */
+    sameSite: string
+  }
+}
+
+export interface ColorModeOptions {
+  /**
+   * Default: `system`
+   */
+  preference: string, // default value of $colorMode.preference
+  /**
+   * Default: `light`
+   */
+  fallback: string, // fallback value if not system preference found
+  /**
+   * Default: `nuxt-color-mode-script`
+   */
+  hid: string,
+  /**
+   * Default: `__NUXT_COLOR_MODE__`
+   */
+  globalName: string,
+  /**
+   * Default: `ColorScheme`
+   */
+  componentName: string,
+  cookie: ColorModeCookieOptions
+}
+
+export interface ColorModeInstance {
+  preference: string,
+  value: string,
+  unknown: boolean
+}
