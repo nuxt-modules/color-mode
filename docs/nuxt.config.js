70aa6d5f01d4a10a7a4cbc9037e8a99ea5f048c8
Sébastien Chopin <seb@nuxtjs.com>
2021-02-15 11:04:37 +0100

    chore: move to plausible

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -4,10 +4,8 @@ export default theme({
   docs: {
     primaryColor: '#00CD82'
   },
-  buildModules: ['nuxt-ackee'],
-  ackee: {
-    server: 'https://ackee.nuxtjs.com',
-    domainId: 'f52f02b3-20f1-49a0-8342-b2112b91405b',
-    detailed: true
+  buildModules: ['vue-plausible'],
+  plausible: {
+    domain: 'color-mode.nuxtjs.org'
   }
 })
