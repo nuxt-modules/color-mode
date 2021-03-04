1a16fbd9ed216ced8c19e8e76712a84fd78fad8f
Shtin Maksim <shtinmn@gmail.com>
2021-03-04 17:10:47 +0300

    fix: divided babel.config for test and prod

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,31 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env', {
-        targets: {
-          browsers: '> 0.25%, not dead, ie 11, ie_mob 11'
-        }
-      }
-    ]
-  ],
-  plugins: [
-    '@babel/plugin-transform-runtime'
-  ]
+  env: {
+    test: {
+      presets: [
+        [
+          '@babel/preset-env', {
+            targets: {
+              esmodules: true
+            }
+          }
+        ]
+      ]
+    },
+    production: {
+      presets: [
+        [
+          '@babel/preset-env', {
+            targets: {
+              browsers: '> 0.25%, not dead, ie 11, ie_mob 11'
+            }
+          }
+        ]
+      ],
+      plugins: [
+        [
+          '@babel/plugin-transform-runtime'
+        ]
+      ]
+    }
+  }
 }
