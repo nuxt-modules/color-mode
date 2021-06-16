dbb6455e0fe18ef744df5ea7fd99c2518f41f614
Daniel Roe <daniel@roe.dev>
2021-06-16 14:47:03 +0100

    test: fill out additional test targets

--- test/csr.test.js
+++ test/csr.test.js
@@ -1,7 +1,8 @@
 import { join } from 'path'
-import { setupTest, get } from '@nuxt/test-utils'
+import { readFile } from 'fs-extra'
+import { setupTest, get, getContext } from '@nuxt/test-utils'
 
-describe('module', () => {
+describe('ssr: false, dev mode', () => {
   const rootDir = join(__dirname, '..', 'example')
 
   setupTest({
@@ -14,6 +15,47 @@ describe('module', () => {
 
   test('render', async () => {
     const { body } = await get('/')
-    expect(body).toContain('nuxt-color-mode')
+    expect(body).toContain('nuxt-color-mode-script')
+  })
+})
+
+describe('ssr: false, target: server, prod mode', () => {
+  const rootDir = join(__dirname, '..', 'example')
+
+  setupTest({
+    server: true,
+    build: true,
+    rootDir,
+    config: {
+      ssr: false,
+      target: 'server'
+    }
+  })
+
+  test('render', async () => {
+    const { body } = await get('/')
+    expect(body).toContain('nuxt-color-mode-script')
+  })
+})
+
+describe('ssr: false, target: static, generated files', () => {
+  const rootDir = join(__dirname, '..', 'example')
+
+  setupTest({
+    generate: true,
+    rootDir,
+    config: {
+      ssr: false
+    }
+  })
+
+  test('generated file', async () => {
+    const { nuxt } = getContext()
+    const generateDir = nuxt.options.generate.dir
+    const files = ['index.html', '200.html']
+    for (const file of files) {
+      const contents = await readFile(join(generateDir, file), 'utf-8')
+      expect(contents).toMatch('nuxt-color-mode-script')
+    }
   })
 })
