acd31c7021630deb35f3eaa15ab6af807597e2b2
Daniel Roe <daniel@roe.dev>
2022-02-21 13:26:38 +0000

    test: update playground to globally register icons

--- playground/nuxt.config.js
+++ playground/nuxt.config.js
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from 'nuxt3'
 import colorModeModule from '..'
 
 export default defineNuxtConfig({
+  components: { global: true },
   css: ['~/assets/main.css'],
   modules: [colorModeModule]
 })
