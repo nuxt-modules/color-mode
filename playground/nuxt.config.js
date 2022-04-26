08680c743eff277d8ff62f38ab6344dc354c366e
Daniel Roe <daniel@roe.dev>
2022-04-26 21:55:48 +0100

    fix: update playground config

--- playground/nuxt.config.js
+++ playground/nuxt.config.js
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 import colorModeModule from '..'
 
 export default defineNuxtConfig({
-  components: { global: true },
+  components: { global: true, dirs: ['~/components'] },
   css: ['~/assets/main.css'],
   modules: [colorModeModule]
 })
