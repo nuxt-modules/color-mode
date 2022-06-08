2c8a0cd5578dbe80693b85d068aa560a5c5b46f0
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-06-08 18:32:26 +0200

    docs: migrate to latest docus

---
+++ docs/nuxt.config.ts
@@ -0,0 +1,37 @@
+import { defineNuxtConfig } from 'nuxt'
+import colors from 'tailwindcss/colors.js'
+
+export default defineNuxtConfig({
+  extends: ['./node_modules/@docus/docs-theme'],
+  github: {
+    owner: 'nuxt-community',
+    repo: 'color-mode-module',
+    branch: 'main'
+  },
+  modules: ['@nuxthq/admin', '@docus/github', 'vue-plausible'],
+  plausible: {
+    domain: 'color-mode.nuxtjs.org'
+  },
+  tailwindcss: {
+    config: {
+      theme: {
+        extend: {
+          colors: {
+            primary: {
+              50: '#d6ffee',
+              100: '#acffdd',
+              200: '#83ffcc',
+              300: '#30ffaa',
+              400: '#00dc82',
+              500: '#00bd6f',
+              600: '#009d5d',
+              700: '#007e4a',
+              800: '#005e38',
+              900: '#003f25'
+            }
+          }
+        }
+      }
+    }
+  }
+})
