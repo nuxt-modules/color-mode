911abdb29d0c1b3eec74ea5466696dbb96727a46
Anthony Fu <anthonyfu117@hotmail.com>
2022-07-06 23:48:05 +0800

    chore: lint

--- src/runtime/composables.ts
+++ src/runtime/composables.ts
@@ -1,5 +1,5 @@
-import type { ColorModeInstance } from './types'
 import { useState } from '#app'
+import type { ColorModeInstance } from './types'
 
 export const useColorMode = () => {
   return useState('color-mode').value as ColorModeInstance
