c10aa04555e34412df289ed48971616c2828c512
Daniel Roe <daniel@roe.dev>
2022-11-24 20:48:19 +0000

    chore: ignore inlining warning

--- build.config.ts
+++ build.config.ts
@@ -1,6 +1,9 @@
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
+  // TODO: fix issue in unbuild with inlining package.json
+  // `Inlined implicit external D:\a\color-mode\color-mode\package.json`
+  failOnWarn: false,
   entries: ['src/script'],
   hooks: {
     'rollup:options' (_ctx, options) {
