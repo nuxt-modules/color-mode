bce7cf88139e67be71717ef84096002fca4a1946
Sébastien Chopin <seb@nuxt.com>
2024-09-13 01:53:29 +0200

    lint fix

--- playground/pages/light.vue
+++ playground/pages/light.vue
@@ -9,7 +9,9 @@ definePageMeta({
 
 <template>
   <div>
-    <h2 class="text-2xl font-bold">This page is forced with light mode</h2>
+    <h2 class="text-2xl font-bold">
+      This page is forced with light mode
+    </h2>
     <p>
       Forcing a color mode is great to implement dark mode to a website incrementally, <a href="https://color-mode.nuxtjs.org/#force-a-color-mode">learn more</a> about it.
     </p>
@@ -20,4 +22,3 @@ definePageMeta({
     </p>
   </div>
 </template>
-
