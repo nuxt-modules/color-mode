c6aa3db939cd2678f3796f724fc52521b254c92d
Sébastien Chopin <seb@nuxt.com>
2024-09-13 13:26:27 +0200

    docs: move to Nuxt UI (#283)

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,38 +1,6 @@
 export default defineAppConfig({
-  docus: {
-    title: 'Nuxt Color Mode',
-    layout: 'default',
-    url: 'https://color-mode.nuxtjs.org',
-    description: 'Dark and Light mode with auto detection made easy with Nuxt 🌗',
-    socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-modules/color-mode',
-    },
-    aside: {
-      level: 1,
-    },
-    image: '/cover.jpg',
-    header: {
-      logo: true,
-    },
-    footer: {
-      credits: {
-        icon: 'IconDocus',
-        text: 'Powered by Docus',
-        href: 'https://docus.com',
-      },
-      iconLinks: [
-        {
-          label: 'Nuxt',
-          href: 'https://nuxt.com',
-          icon: 'IconNuxt',
-        },
-        {
-          label: 'Vue Telescope',
-          href: 'https://vuetelescope.com',
-          icon: 'IconVueTelescope',
-        },
-      ],
-    },
+  ui: {
+    primary: 'green',
+    gray: 'slate',
   },
 })
--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,7 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['@nuxt-themes/docus'],
-  modules: ['@nuxtjs/plausible'],
-  imports: {
-    autoImport: true,
-  },
+  compatibilityDate: '2024-09-13',
+  extends: '@nuxt/ui-pro',
+  modules: ['@nuxt/ui', '@nuxt/content'],
+  uiPro: { license: 'oss' },
+  devtools: { enabled: true },
 })
