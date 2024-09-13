2b971b6f0b8d55876cf3e9794b43fedab3b64716
Sébastien Chopin <seb@nuxt.com>
2024-09-13 13:21:46 +0200

    up

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  compatibilityDate: '2024-09-13',
   extends: '@nuxt/ui-pro',
   modules: ['@nuxt/ui', '@nuxt/content'],
-  compatibilityDate: '2024-09-13',
+  uiPro: { license: 'oss' },
+  devtools: { enabled: true },
 })
