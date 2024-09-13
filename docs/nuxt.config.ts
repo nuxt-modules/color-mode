fbde0a125d0040905d7ded10157377d53bab5d51
Sébastien Chopin <seb@nuxt.com>
2024-09-13 13:52:59 +0200

    docs: add back plausible

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-09-13',
   extends: '@nuxt/ui-pro',
-  modules: ['@nuxt/ui', '@nuxt/content'],
+  modules: ['@nuxt/ui', '@nuxt/content', '@nuxtjs/plausible'],
   uiPro: { license: 'oss' },
   devtools: { enabled: true },
 })
