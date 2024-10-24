9df327fdf8d33bf8c780566f2859478a072de97c
Daniel Roe <daniel@roe.dev>
2024-10-24 12:04:26 +0100

    chore: lint configs

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-09-13',
   extends: '@nuxt/ui-pro',
   modules: ['@nuxt/ui', '@nuxt/content', '@nuxtjs/plausible'],
-  uiPro: { license: 'oss' },
   devtools: { enabled: true },
+  compatibilityDate: '2024-09-13',
+  uiPro: { license: 'oss' },
 })
