ea61420dee0c0d0f0e9e715eeb37a9272572d8b6
Benjamin Canac <canacb1@gmail.com>
2026-02-11 17:28:11 +0100

    fix(light): consistent classes

--- playground/pages/light.vue
+++ playground/pages/light.vue
@@ -15,13 +15,13 @@ definePageMeta({
     <p class="py-5">
       Forcing a color mode is great for implementing dark mode on a website incrementally. <a
         href="https://color-mode.nuxtjs.org/#force-a-color-mode"
-        class="text-emerald-600 dark:text-emerald-400 underline"
+        class="text-emerald-600 dark:text-emerald-400 sepia:text-[#6b4c2a] underline"
       >Learn more</a>.
     </p>
-    <p>
+    <p class="pb-5">
       <NuxtLink
         to="/"
-        class="text-emerald-600 dark:text-emerald-400 underline"
+        class="text-emerald-600 dark:text-emerald-400 sepia:text-[#6b4c2a] underline"
       >
         Home
       </NuxtLink>
